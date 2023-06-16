fb88ed26efb0b15b2735605d7abacc525d2d1a08
Ngobidi <chibuzormaxwell@gmail.com>
2023-06-16 08:09:27 -0700

    1-last_digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet
+ *
+ * Return: return 0 (if its correct)
+ */
+int main(void)
+{
+	char i = (lower case a-z);
+	int i;
+
+        for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
