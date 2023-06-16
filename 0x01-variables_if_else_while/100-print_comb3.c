381b11511ff4cb010632d9b6fbef7e3967e45ff3
Ngobidi <chibuzormaxwell@gmail.com>
2023-06-16 16:42:18 -0700

    advanced task10_11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Function Write all possible different combinations of two digits
+ * Return: Return 0 (when check is completed)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
