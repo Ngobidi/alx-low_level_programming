22da18d6e99d45f10dd7609a1d39d9ccd8d8b2d8
Ngobidi <chibuzormaxwell@gmail.com>
2023-06-16 15:45:30 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function numbers
+ *
+ * Return: Return 0 (if check is completed)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
