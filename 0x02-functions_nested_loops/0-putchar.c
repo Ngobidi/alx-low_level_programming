8e1b33c82a9ffa2d85d9443d04387e8fdc3a7ebf
Ngobidi <chibuzormaxwell@gmail.com>
2023-06-20 10:50:50 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include "main.h"
-
 /**
- * main - print function putchar
- *
- * Return: always 0 (when validated) 
+ * main - write function  _putchar
+ * 
+ * Return: always 0 (when validated)
  */
 
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
