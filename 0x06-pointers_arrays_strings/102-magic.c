dc609831e31c9953808e689a20df48c52a82ab49
Ngobidi <chibuzormaxwell@gmail.com>
2023-06-29 11:20:14 -0700

    correction 1

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,7 +7,7 @@ int main(void)
   int *p;
 
   a[2] = 1024;
-  p = &n;
+  p = &a[2];
   /*
    * print your line of code
    * note:
@@ -16,7 +16,7 @@ int main(void)
    * - write only one statement
    * - not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+  *(p) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
