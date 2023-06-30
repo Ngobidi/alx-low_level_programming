d6e0a296f85c8ad87e548a8ee945e8c54550aac6
Ngobidi <chibuzormaxwell@gmail.com>
2023-06-30 02:42:04 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,8 +16,8 @@ int main(void)
    * - write only one statement
    * - not allowed to code anything else than this line of code
    */
-  *(p + 3) = 98;
+  (p + 3) = 98;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", *(p + 3));
+  printf("a[2] = %d\n", *(p + 5));
   return (0);
 }
