1688ea027c2c8d61da3ebe7efdfb23d31e2c175f
Ngobidi <chibuzormaxwell@gmail.com>
2023-07-03 09:17:11 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,7 +6,7 @@
  * @s: string to find
  * @accept: string containing the bytes to find
  *
- * Return: return pointer to the byte in s that matches one of the bytes in accept
+ * Return: pointer to the byte in s that matches one of the bytes in accept
  * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
