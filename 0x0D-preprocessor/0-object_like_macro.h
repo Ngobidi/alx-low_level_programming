c1ec23b8c549ce25ae58ad97526ac3707e6e153d
Ngobidi <chibuzormaxwell@gmail.com>
2023-07-17 07:28:20 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef Oject_Like_Marco_H
+#define Object_Like_Marco_H
+
+#define Size 1024
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - state the file name.
+ *
+ * Return: return 0 (after execution)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
