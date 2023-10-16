20b6b09ab7f2144321d96ffb3f2bfcf723a9177f
Ngobidi <chibuzormaxwell@mail.com>
2023-10-16 08:59:04 -0700

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp https://github.com/Ngobidichibuzor/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
