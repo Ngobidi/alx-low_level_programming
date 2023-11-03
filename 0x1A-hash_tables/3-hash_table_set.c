199266e3d0e37dd4b2bf430697637f76d7d34ce5
Ngobidi <chibuzormaxwell@mail.com>
2023-11-03 04:37:15 -0700

    Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,34 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
+ * hash_table_set - insert an element in a hash_table.
+ * @ht: A hash_table.
+ * @key: The key to be added in the list
+ * @value: The value of the element to add in the list
  *
- * Return: Upon failure - 0.
- *         Otherwise - 1.
+ * Return: 1 on successful, else 0
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index, i;
+	char *dublicate;
+	unsigned long int index, j;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	dublicate = strdup(value);
+	if (dublicate == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	for (j = index; ht->array[j]; j++)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(ht->array[j]->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			free(ht->array[j]->value);
+			ht->array[j]->value = dublicate;
 			return (1);
 		}
 	}
@@ -36,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		free(value_copy);
+		free(dublicate);
 		return (0);
 	}
 	new->key = strdup(key);
@@ -45,7 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-	new->value = value_copy;
+	new->value = dublicate;
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
